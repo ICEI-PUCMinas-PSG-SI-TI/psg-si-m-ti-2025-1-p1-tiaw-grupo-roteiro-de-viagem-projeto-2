269d771d9c19e97b34d3d2629b7bc77da5d972eb
Karen Dornas Oliveira <karendornas8@gmail.com>
2025-06-18 16:41:43 -0300

    Teste

--- src/public/assets/scripts/usuario.js
+++ src/public/assets/scripts/usuario.js
@@ -1,13 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
             const nomelogin = document.getElementById("nomelogin");
+            const nomeusuario = document.getElementById("nomeUsuarioComentario");
             const usuarioSalvo = sessionStorage.getItem("usuarioCorrente");
 
             if (usuarioSalvo) {
                 const usuario = JSON.parse(usuarioSalvo);
-                nomelogin.textContent = usuario.login || usuario.nomecompleto || "Usuário";
+                nomelogin.textContent = (usuario.login || usuario.nomecompleto || "Usuário").toUpperCase();
                 nomelogin.style.cursor = "pointer";
                 nomelogin.removeEventListener('click', irparaLogin);
                 nomelogin.addEventListener('click', irparaPerfil);
+                nomeusuario.textContent = (usuario.login || usuario.nomecompleto || "Usuário").toUpperCase();
             } else {
                 nomelogin.textContent = "LOGIN";
                 nomelogin.style.cursor = "pointer";
